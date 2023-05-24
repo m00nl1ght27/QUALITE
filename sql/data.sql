f8ee52e6aa255340a104ba131c1c045f0308455e
m00nl1ght <95619584+m00nl1ght27@users.noreply.github.com>
2023-05-24 10:30:16 +0200

    Add files via upload

---
+++ sql/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO clients (nom, prenom, date_naissance, adresse, cp, ville) VALUES
+  ('Doe', 'John', '1990-05-15', '123 Rue de la Poste', '12345', 'Ville1'),
+  ('Smith', 'Jane', '1985-09-21', '456 Avenue du Soleil', '54321', 'Ville2'),
+  ('Johnson', 'Robert', '1978-12-01', '789 Boulevard des Fleurs', '98765', 'Ville3');
